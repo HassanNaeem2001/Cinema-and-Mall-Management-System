cb7c58bcc281a4256d53257afe0b0ab37542046f
Hassan Naeem <hassannaeem4590@gmail.com>
2025-11-24 19:08:41 +0500

    24th Nov 2025 - Auth and Admin Integrated

---
+++ resources/views/admin/allusers.blade.php
@@ -0,0 +1,6 @@
+@extends('admin.layout')
+@section('admincontent')
+<div class="content-wrapper">
+    <h3>Record of All Users</h3>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/adm', function () {
+    return view('admin.dashboard');
+});
+Route::get('/allusers', function () {
+    return view('admin.allusers');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
